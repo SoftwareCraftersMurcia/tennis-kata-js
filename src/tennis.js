faaf9c9a79ccbc960e8599c8682cfb00c775f6be
Luis Rovirosa Mairlot <luis@codium.team>
2020-11-20 18:50:04 +0100

    refactor tennis

--- src/tennis.js
+++ src/tennis.js
@@ -1,16 +1,16 @@
 class Tennis {
     score(player1Points, player2Points) {
-        const points = ['fifteen', 'thirty', 'forty', 'love'];
+        const points = ['love', 'fifteen', 'thirty', 'forty'];
         if (player1Points === 1) {
-            return points[0] + '-love';
-        }
-        if (player1Points === 2) {
             return points[1] + '-love';
         }
-        if (player1Points === 3) {
+        if (player1Points === 2) {
             return points[2] + '-love';
         }
-        return points[3] + '-love';
+        if (player1Points === 3) {
+            return points[3] + '-love';
+        }
+        return points[0] + '-love';
     }
 }
 
